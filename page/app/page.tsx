1825d4dc4decd1e2adb5ae6884ea699bd03ec38f
MauchiB <166924790+MauchiB@users.noreply.github.com>
2026-01-01 02:00:53 +0300

    Delete page directory

--- page/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ChatList } from "@/components/chat-list"
-
-export default function Page() {
-  return (
-    <div className="min-h-screen bg-background">
-      <ChatList />
-    </div>
-  )
-}
